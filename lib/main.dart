a1a0a0305c5c33e4a3af9c36fa4be7ba980d0455
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-23 17:51:03 +0200

    add internet permission to manifest

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,72 @@ void main() {
   runApp(
     const MaterialApp(
       title: 'Globe Factual',
+      home: MyContent(),
     ),
   );
 }
 
-Future<void> fetchCountry() async {
-  final response =
-      await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
+Future<Country> fetchCountry() async {
+  final response = await http
+      .get(Uri.parse('https://restcountries.com/v3.1/name/deutschland'));
 
   if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
-    print(data);
+    return Country.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('This failed');
+  }
+}
+
+class Country {
+  final String name;
+
+  const Country({
+    required this.name,
+  });
+
+  factory Country.fromJson(List<dynamic> jsonList) {
+    if (jsonList.isEmpty) {
+      throw Exception('No data in list');
+    }
+    final firstItem = jsonList.first;
+    if (firstItem is Map<String, dynamic>) {
+      return Country(name: firstItem['name'].common as String);
+    } else {
+      throw Exception('Something is wrong ');
+    }
+  }
+}
+
+class MyContent extends StatefulWidget {
+  const MyContent({super.key});
+
+  @override
+  State<MyContent> createState() => _MyContentState();
+}
+
+class _MyContentState extends State<MyContent> {
+  late Future<Country> futureCountry;
+
+  @override
+  void initState() {
+    super.initState();
+    futureCountry = fetchCountry();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<Country>(
+        future: futureCountry,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data!.name);
+          } else if (snapshot.hasError) {
+            return const Text('No data found ');
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
   }
 }
--- test/fetch_test.dart
+++ test/fetch_test.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> fetchCountry() async {
-  final response =
-      await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
+  final response = await http
+      .get(Uri.parse('https://restcountries.com/v3.1/name/deutschland'));
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
